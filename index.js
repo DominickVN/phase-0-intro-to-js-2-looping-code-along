5f83a335744c5bb6868a37d93f11a459d3e9c1d2
dominick <vannusdominick@gmail.com>
2023-02-15 15:04:20 -0800

    commit tree 9cca3d3d904a3e871b0ec72d37b7a4b03225be7d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const event = "amazing";
+
+function writeCards(names, event) {
+    const writtenCards = []
+    for (let i = 0; i < names.length; i += 1) {
+        writtenCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return writtenCards
+}
+
+function countDown(){
+    let numCountdown=11;
+    while(numCountdown--){
+      console.log(numCountdown)
+    }
+  } 
